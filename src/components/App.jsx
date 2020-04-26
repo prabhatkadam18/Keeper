8d88118a1acb8857e573cc6f2d8ce9a0d4150b8b
Prabhat Kadam <31852847+prabhatkadam18@users.noreply.github.com>
2020-04-26 17:00:29 +0530

    Dynamically rendered Notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import notes from "../notes";
 
 function App(){
   return (
     <div>
       <Header/>
-      <Note/>
+      {notes.map(note=>
+        <Note
+          key={note.key}
+          title={note.title}
+          content={note.content}          
+        />
+      )}
       <Footer/>
     </div>
   );
