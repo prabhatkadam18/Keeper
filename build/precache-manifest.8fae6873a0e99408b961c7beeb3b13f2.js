07ed1de380a3d8ec0a15146ab897bf65915ef4ca
Prabhat Kadam <prabhatkadam18@gmail.com>
2020-08-27 12:20:13 +0530

    changes to deploy

---
+++ build/precache-manifest.8fae6873a0e99408b961c7beeb3b13f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181057af33945cd098065701043ddefb",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "bd028030cbd7c86438ef",
+    "url": "/Keeper/static/js/2.bdf612af.chunk.js"
+  },
+  {
+    "revision": "b256b44d0256ad2b309b",
+    "url": "/Keeper/static/js/main.14daa704.chunk.js"
+  },
+  {
+    "revision": "0b19d6945c0f25e6047a",
+    "url": "/Keeper/static/js/runtime-main.c9835c2b.js"
+  }
+]);
